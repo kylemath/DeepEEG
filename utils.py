bdaaa974178f2406c84ca155cd902a653c47eb49
Kyle Mathewson <kylemath@gmail.com>
2019-02-02 13:13:26 -0700

    fixed bug

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def SetupTensorFlow():
   get_ipython().system_raw('./ngrok http 6007 &')
 
   #get url
-  ! curl -s http://localhost:4040/api/tunnels | python3 -c \
+  curl -s http://localhost:4040/api/tunnels | python3 -c \
       "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
 
 
