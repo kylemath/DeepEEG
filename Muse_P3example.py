525aa86717d8a6cd6f62cc0ccea91e37983f6940
Kyle Mathewson <kylemath@gmail.com>
2019-03-03 12:41:45 -0700

    saved muse example

--- Muse_P3example.py
+++ Muse_P3example.py
@@ -6,10 +6,7 @@
 #python
 from utils import *
 data_dir = 'visual/cueing'
-subs = [101, 102, 103, 104, 105, 106, 108, 109, 110, 111, 112,
-        202, 203, 204, 205, 207, 208, 209, 210, 211,
-        301, 302, 303, 304, 305, 306, 307, 308, 309]
-        subs = [101, 102, 103, 104]
+subs = [101, 102, 103, 104]
 nsesh = 2
 event_id = {'LeftCue': 1,'RightCue': 2}
 #Load Data
