dfafcc1eecb26500a5b1094adfa63818b624b40b
Kyle Mathewson <kylemath@gmail.com>
2019-03-23 21:32:25 -0600

    trying to figure out how to separate conditions and sessions and subjects

--- newP3_exampleBV.py
+++ newP3_exampleBV.py
@@ -1,40 +1,36 @@
-#conda create -n deepeeg
-#source activate deepeeg
-#chomd +x install.sh
-#bash install.sh
-#!git clone https://github.com/kylemath/eeg-notebooks
-#python
 from utils import *
-data_dir = '/Users/kylemathewson/Desktop/data/'
+data_dir = '/Users/kylemathewson/data/'
 exp = 'P3'
 subs = ['001','002','004','005','006','007','008','010']
-#subs = [ '002']
+subs = [ '002']
 
 sessions = ['ActiveDry','ActiveWet','PassiveWet']
-epochs = []
-for isesh, session in enumerate(sessions):
-	event_id = {(session + '/Target'): 1, (session + '/Standard'): 2}
+epochs = {} #dict
+for session in sessions:
+	event_id = {'Target': 1, 'Standard': 2}
 	for sub in subs:
 		print('Loading data for subject ' + sub)
 		#Load Data
 		raw = LoadBVData(sub,session,data_dir,exp)
 		#Pre-Process EEG Data
-		temp_epochs = PreProcess(raw,event_id,
-							emcp=True, rereference=True,
+		epochs[session] = PreProcess(raw,event_id,
+							emcp_epochs=True, rereference=True,
 							plot_erp=False, rej_thresh_uV=250, 
 							epoch_time=(-1,2), baseline=(-1,-.5) )
-		if len(temp_epochs) > 0:
-			epochs.append(temp_epochs)
-		else:
+		if len(epochs[session]) == 0:
 			print('Sub ' + sub + ', Cond ' 
 					+ session + 'all trials rejected')
 
-
-
-print(epochs)
-epochs = concatenate_epochs(epochs)	
 print(epochs)
 
+#create evoked dict by averaging all the epochs for this test subject
+evoked_dict = {}
+for session in sessions: 
+	evoked_dict[session '/' 'Target'] = epochs[session]['Target'].average()
+	evoked_dict[session '/' 'Standard'] = epochs[session]['Standard'].average()
+
+#plot all the erps on one plot
+viz.plot_compare_evokeds(evoked_dict,picks=[6])
 
 
 
