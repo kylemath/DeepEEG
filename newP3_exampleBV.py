7ddb3523f33c71a0b9c644473da2651be62d15fe
Kyle Mathewson <kylemath@gmail.com>
2019-03-23 22:47:30 -0600

    plot 3 sessions with 2 conditions on single plot over subjects working

--- newP3_exampleBV.py
+++ newP3_exampleBV.py
@@ -2,37 +2,35 @@ from utils import *
 data_dir = '/Users/kylemathewson/data/'
 exp = 'P3'
 subs = ['001','002','004','005','006','007','008','010']
-subs = [ '002','002']
-
 sessions = ['ActiveDry','ActiveWet','PassiveWet']
+
 event_id = {'Target': 1, 'Standard': 2}
 epochs = {} #dict
-all_evoked = []
-for sub in subs:
-	for session in sessions:
+all_evokeds = {}
+fig = plt.figure()
+
+for i_session, session in enumerate(sessions):
+	all_lists = {'Target':[], 'Standard':[]}
+	for sub in subs:
 		print('Loading data for subject ' + sub)
 		#Load Data
 		raw = LoadBVData(sub,session,data_dir,exp)
 		#Pre-Process EEG Data
 		epochs[session] = PreProcess(raw,event_id,
 							emcp_epochs=True, rereference=True,
-							plot_erp=False, rej_thresh_uV=250, 
-							epoch_time=(-1,2), baseline=(-1,-.5) )
-		if len(epochs[session]) == 0:
-			print('Sub ' + sub + ', Cond ' 
-					+ session + 'all trials rejected')
-	
-	#create evoked dict by averaging all the epochs for this test subject
-	evoked_dict = {}
-	for session in sessions: 
-		evoked_dict[session + '/' + 'Target'] = epochs[session]['Target'].average()
-		evoked_dict[session + '/' + 'Standard'] = epochs[session]['Standard'].average()
-	all_evoked.append(evoked_dict)
+							plot_erp=False, rej_thresh_uV=500, 
+							epoch_time=(-.2,1), baseline=(-.2,0) )
+		#create evoked dict by averaging all the epochs for this test subject
+		all_lists['Target'].append(epochs[session]['Target'].average())
+		all_lists['Standard'].append(epochs[session]['Standard'].average())
+	#create the dict with condition keys and values are lists over subs	
+	all_evokeds['Target'] = all_lists['Target']
+	all_evokeds['Standard'] = all_lists['Standard']
+	ax = plt.subplot(3,1,i_session+1)
+	viz.plot_compare_evokeds(all_evokeds,picks=[6],axes=ax,
+							title=session,show=False)
+plt.show()
 
-
-#crash - TypeError: All evokeds entries  must be an instance of Evoked, got <type 'str'> instead
-#plot all the erps on one plot
-viz.plot_compare_evokeds(all_evoked,picks=[6])
 
 
 
